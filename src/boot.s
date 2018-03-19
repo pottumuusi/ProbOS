bdc37797119c96bee35815ba609d34068931de46
Aleksi Backman <bddubbb@gmail.com>
2018-03-19 22:59:17 +0200

    Add label for error printing

--- src/boot.s
+++ src/boot.s
@@ -6,3 +6,12 @@ start:
     ; print `OK` to screen
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
+
+; Print "ERR: <error code>" to screen and hang
+; param: error code (ascii) in al
+error:
+    mov dword [0xb8000], 0x4f524f45
+    mov dword [0xb8004], 0x4f3a4f52
+    mov dword [0xb8008], 0x4f204f20
+    mov byte [0xb800a], al
+    hlt
