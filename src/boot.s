de255689f12c4050e20e2748bc5f117821c0231e
Aleksi Backman <bddubbb@gmail.com>
2018-03-19 23:45:53 +0200

    Add cpuid support check

--- src/boot.s
+++ src/boot.s
@@ -5,6 +5,7 @@ bits 32
 start:
     mov esp, stack_top
     call check_multiboot
+    call check_cpuid
 
 main:
     ; print `OK` to screen
@@ -13,12 +14,49 @@ main:
 
 check_multiboot:
     ; Multiboot specification states that a compliant bootloader must write
-    ; magic value 0x36d76289 to eax before loading a kernel.
+    ; magic value 0x36d76289 to EAX before loading a kernel.
     cmp eax, 0x36d76289
     jne .no_multiboot
     ret
 .no_multiboot:
     mov al, "0"
+    jmp error
+
+check_cpuid:
+    ; Check if CPUID is supported by attempting to flip the ID bit (bit 21)
+    ; in the FLAGS register. If we can flip it, CPUID is available.
+
+    ; Copy FLAGS in to EAX via stack
+    pushfd
+    pop eax
+
+    ; Copy to ECX as well for comparing
+
+    mov ecx, eax
+
+    ; Flip the ID bit
+    xor eax, 1 << 21
+
+    ; Copy EAX to FLAGS via the stack
+    push eax
+    popfd
+
+    ; Copy FLAGS back to EAX (with the flipped bit if CPUID is supported)
+    pushfd
+    pop eax
+
+    ; Restore FLAGS from the old version stored in ECX (i.e. flipping the
+    ; ID bit back if it was ever flipped).
+    push ecx
+    popfd
+
+    ; Compare EAX and ECX. If they are equal then that means the bit wasn't
+    ; flipped, and CPUID isn't supported.
+    cmp eax, ecx
+    je .no_cpuid
+    ret
+.no_cpuid:
+    mov al, "1"
     jmp error
 
 ; Print "ERR: <error code>" to screen and hang
