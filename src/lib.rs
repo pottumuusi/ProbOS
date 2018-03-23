82fc86c09102edafc72843e2190d7aa44dd611f4
Aleksi Backman <bddubbb@gmail.com>
2018-03-24 01:24:16 +0200

    Rust print Hello World
    Outcomment code as kernel seems to have issues when amount
    of variables does increase.

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,29 @@ extern crate rlibc;
 
 #[no_mangle]
 pub extern fn rust_main() {
-    let x = ["Hello", "World", "!"];
-    let y = x;
+    // NOTE: Stack is small and there is no guard page
 
-    let test = (0..3).flat_map(|x| 0..x).zip(0..);
+    let hello = b"Hello World!";
+    let color_byte = 0x1f; // White foreground, blue background
+
+    let mut hello_colored = [color_byte; 24];
+    for (i, char_byte) in hello.into_iter().enumerate() {
+        hello_colored[i*2] = *char_byte;
+    }
+
+    // Write "Hello World!" to the center of the VGA text buffer
+    let buffer_ptr = (0xb8000 + 1988) as *mut _;
+    unsafe { *buffer_ptr = hello_colored };
+
+    loop{}
 }
 
 #[lang = "eh_personality"] #[no_mangle] pub extern fn eh_personality() {}
-#[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! { loop{} }
+#[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! {
+    let buffer_ptr = (0xb8000) as *mut _;
+    let red = 0x4f;
+    unsafe {
+        *buffer_ptr = [b'P', red, b'a', red, b'n', red, b'i', red, b'c', red, b'!', red];
+    };
+    loop{}
+}
